e6c6a99f20bf9effaf9e1708f0861ccc76e9b915
vyruss <vyruss@gmail.com>
2020-11-23 13:07:34 -0600

    first commit

---
+++ incubator/src/main/java/com/staging/incubator/IncubatorApplication.java
@@ -0,0 +1,13 @@
+package com.staging.incubator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubatorApplication.class, args);
+	}
+
+}
